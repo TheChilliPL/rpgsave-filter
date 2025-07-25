fd7d033e086d84abfe58861e1bb183600d3ed04b
Chilli <chilli@thechilli.dev>
2025-07-25 20:26:50 +0200

    Add automatic installation subcommand

--- README.md
+++ README.md
@@ -2,15 +2,44 @@
 
 Git filter driver for RPG maker MV/MZ save files!
 
-## Usage
+## Installation
 
-Add this filter driver to your `.gitconfig` or the `.git/config` file of the project:
+### Automatic
+
+You need to build this tool from source (`cargo build`) or otherwise get the executable for your system.
+You can use [`cargo binstall rpgsave-filter`](<https://github.com/cargo-bins/cargo-binstall?tab=readme-ov-file#installation>)
+to get the executable without building it locally.
+
+Then, in the directory of the repo you want to install it to, simply run:
+```shell
+rpgsave-filter install
+```
+
+This will automatically add this driver to the project's `.git/config` with the same
+path that you used to call it.
+
+### Manual
+
+First, add this filter driver to your `.gitconfig` or the `.git/config` file of the project:
 ```
 [filter "rpgsave"]
     clean = <PATH TO RPGSAVE-FILTER EXECUTABLE> clean
     smudge = <PATH TO RPGSAVE-FILTER EXECUTABLE> smudge
+    required = true
 ```
-and set `.rpgsave` files to use it by appending to .gitattributes:
+
+If this is done in a repository that already had it used, be sure to clear the index and
+check out the repository again:
+```
+rm .git/index
+git checkout HEAD -- .
+```
+
+## Usage
+
+You can set `.rpgsave` files to use it by appending to `.gitattributes`:
 ```
 *.rpgsave filter=rpgsave
 ```
+
+Git will automatically use this filter whenever it's necessary.
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
+use std::fs::remove_file;
 use std::io;
 use std::io::{Read, Write};
+use std::process::{Command as StdCommand};
 use clap::{Parser, Subcommand};
-use eyre::{Context, OptionExt};
+use eyre::{Context, ContextCompat, OptionExt};
 
 #[derive(Parser, Debug)]
 #[command(author, version, about)]
@@ -16,16 +18,20 @@ enum Command {
     Smudge,
     /// Used when staging the file. Decodes the save data to readable and formatted JSON.
     Clean,
+    /// Installs the filter to the current git repository.
+    ///
+    /// It will use the path to rpgsave-filter that was used to call this command.
+    Install,
 }
 
 fn main() -> eyre::Result<()> {
     let cli = Cli::parse();
 
-    let mut input = String::with_capacity(10*1024);
-    io::stdin().read_to_string(&mut input)?;
-
     match cli.command {
         Command::Smudge => {
+            let mut input = String::with_capacity(10*1024);
+            io::stdin().read_to_string(&mut input)?;
+
             // Input is formatted JSON
             let data: serde_json::Value = serde_json::from_str(&input)?;
 
@@ -36,6 +42,9 @@ fn main() -> eyre::Result<()> {
             io::stdout().write_all(compressed_base64.as_bytes())?;
         },
         Command::Clean => {
+            let mut input = String::with_capacity(10*1024);
+            io::stdin().read_to_string(&mut input)?;
+
             // Input is base64
             let minified_json_utf16 = lz_str::decompress_from_base64(&input)
                 .ok_or_eyre("Failed to decompress base64")?;
@@ -56,6 +65,74 @@ fn main() -> eyre::Result<()> {
 
             // Output is formatted JSON
             io::stdout().write_all(formatted_json.as_bytes())?;
+        },
+        Command::Install => {
+            let this_path = std::env::current_exe().wrap_err("Failed to get current executable path")?;
+            let this_path_str = this_path.to_str().wrap_err("Failed to convert current executable path to string")?;
+            let this_path_str = this_path_str.replace("\\", "/");
+
+            let cmd_status = StdCommand::new("git")
+                .arg("status")
+                .arg("--porcelain=1")
+                .output().wrap_err("Failed to get git status")?;
+
+            if !cmd_status.status.success() {
+                return Err(eyre::eyre!("Failed to get git status"));
+            }
+
+            if !cmd_status.stdout.is_empty() {
+                return Err(eyre::eyre!("Git repository is not clean. Please commit or stash changes before installing the filter."));
+            }
+
+            println!("Installing filter to current git repository");
+            let cmd_clean = StdCommand::new("git")
+                .arg("config")
+                .arg("--local")
+                .arg("filter.rpgsave.clean")
+                .arg(format!("{} clean", this_path_str))
+                .status().wrap_err("Failed to set clean filter")?;
+
+            if !cmd_clean.success() {
+                return Err(eyre::eyre!("Failed to set clean filter"));
+            }
+
+            let cmd_smudge = StdCommand::new("git")
+                .arg("config")
+                .arg("--local")
+                .arg("filter.rpgsave.smudge")
+                .arg(format!("{} smudge", this_path_str))
+                .status().wrap_err("Failed to set smudge filter")?;
+
+            if !cmd_smudge.success() {
+                return Err(eyre::eyre!("Failed to set smudge filter"));
+            }
+
+            let cmd_required = StdCommand::new("git")
+                .arg("config")
+                .arg("--local")
+                .arg("filter.rpgsave.required")
+                .arg("true")
+                .status().wrap_err("Failed to set filter as required")?;
+
+            if !cmd_required.success() {
+                return Err(eyre::eyre!("Failed to set filter as required"));
+            }
+
+            println!("Checking out repository");
+            remove_file(".git/index")?;
+
+            let cmd_checkout = StdCommand::new("git")
+                .arg("checkout")
+                .arg("HEAD")
+                .arg("--")
+                .arg(".")
+                .status().wrap_err("Failed to checkout repository")?;
+
+            if !cmd_checkout.success() {
+                return Err(eyre::eyre!("Failed to checkout repository"));
+            }
+
+            println!("Successfully installed filter");
         },
     }
 
